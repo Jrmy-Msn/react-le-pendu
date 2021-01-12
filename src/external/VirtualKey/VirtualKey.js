524bc5d7741379b104c806a707d4338863e98b31
Jrmy Msn <nc>
2021-01-12 16:52:28 +0100

    Externalisation du clavier virtuel

---
+++ src/external/VirtualKey/VirtualKey.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import './VirtualKey.css'
+
+const TAG = '[VirtualKey]'
+
+const VirtualKey = ({value, feedback, onClick, onMouseOver, onMouseOut}) => {
+  return (
+    <div
+      role="button"
+      aria-roledescription="Touche de clavier virtuel"
+      aria-label={`Lettre ${value}`}
+      className={`VirtualKey ${feedback}`}
+      onMouseOut={() => onMouseOut(value)}
+      onMouseOver={() => onMouseOver(value)}
+      onClick={() => onClick(value)}>{value}</div>
+  )
+}
+
+VirtualKey.propTypes = {
+  value: PropTypes.string.isRequired,
+  feedback: PropTypes.string,
+  onClick: PropTypes.func,
+  onMouseOver: PropTypes.func,
+  onMouseOut: PropTypes.func,
+}
+
+VirtualKey.defaultProps = {
+  onClick: () => void undefined,
+  onMouseOver: () => void undefined,
+  onMouseOut: () => void undefined,
+  feedback: '',
+}
+
+
+export default VirtualKey
